6e456b4e89e3f9f098d1b240856a8f1cb73acc78
Paul Arabatzis <paul.arabatzis@maine.edu>
2020-12-30 11:25:30 -0500

    fixing

--- loan_service/admin.py
+++ loan_service/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from .models import Loan, payment_log
+from .models import Loan, PaymentLog
 
 admin.site.register(Loan)
-admin.site.register(payment_log)
+admin.site.register(PaymentLog)
